8bb31c3512c23553d7935d6bcd4b3b9fa80d5ef4
None <None>
2017-07-14 01:00:35 +0000

    Draft of test for optimal k

--- tests/dctcp_tests.py
+++ tests/dctcp_tests.py
@@ -66,7 +66,7 @@ def dctcp_queue_test(use_dctcp, queue_results_file, throughout_results_file,
     print("Starting iperf clients...")
     for s in senders:
         print("%s sending to %s" % (s.IP(), receiver.IP()))
-        s.popen("iperf -c %s -w 32m -t %d -i 1 > %s/%s-%s" % (receiver.IP(),
+        s.popen("iperf -c %s -w 32m -t %d -i 0.1 > %s/%s-%s" % (receiver.IP(),
                 (time + 30), RESULTS_DIR, s.name, throughout_results_file),
                 shell=True)
 
@@ -115,13 +115,12 @@ def dctcp_convergence_test(use_dctcp, results_file, bw=100, num_flows=5,
     receiver.popen("iperf -s -w 32m")
 
     print("Starting iperf clients...")
-    # need to fix this...
     i = num_flows
     for s in senders:
         run_time = (i * 2 - 1) * interval_time
         print("%s sending to %s - run time: %d" %
               (s.name, receiver.name, run_time))
-        s.popen("iperf -c %s -w 32m -t %d -i 5 > %s/%s-%s &" %
+        s.popen("iperf -c %s -w 32m -t %d -i 0.1 > %s/%s-%s &" %
                 (receiver.IP(), run_time, RESULTS_DIR, s.name, results_file),
                 shell=True)
         sleep(interval_time)
@@ -137,50 +136,60 @@ def dctcp_convergence_test(use_dctcp, results_file, bw=100, num_flows=5,
 if __name__ == '__main__':
     setLogLevel('info')
 
-    # # test queue size with two flows over a shared bottleneck
-    # print "queue test: tcp reno - 2 flows"
-    # dctcp_queue_test(
-    #     use_dctcp=False,
-    #     queue_results_file="reno_queue_2.csv",
-    #     throughout_results_file="reno_thru_2.csv",
-    #     bw=1000,
-    #     num_flows=2,
-    #     time=5)
+    # test queue size with two flows over a shared bottleneck
+    print "queue test: tcp reno - 2 flows"
+    dctcp_queue_test(
+        use_dctcp=False,
+        queue_results_file="reno_queue_2.csv",
+        throughout_results_file="reno_thru_2.csv",
+        bw=1000,
+        num_flows=2,
+        time=5)
 
-    # print "queue test: dctcp - 2 flows"
-    # dctcp_queue_test(
-    #     use_dctcp=True,
-    #     queue_results_file="dctcp_queue_2.csv",
-    #     throughout_results_file="dctcp_thru_2.csv",
-    #     bw=1000,
-    #     num_flows=2,
-    #     time=5)
+    print "queue test: dctcp - 2 flows"
+    dctcp_queue_test(
+        use_dctcp=True,
+        queue_results_file="dctcp_queue_2.csv",
+        throughout_results_file="dctcp_thru_2.csv",
+        bw=1000,
+        num_flows=2,
+        time=5)
 
-    # # test queue size with twenty flows over a shared bottleneck
-    # print "queue test: tcp reno - 20 flows"
-    # dctcp_queue_test(
-    #     use_dctcp=False,
-    #     queue_results_file="reno_queue_20.csv",
-    #     throughout_results_file="reno_thru_20.csv",
-    #     bw=1000,
-    #     num_flows=20,
-    #     time=5)
+    # test queue size with twenty flows over a shared bottleneck
+    print "queue test: tcp reno - 20 flows"
+    dctcp_queue_test(
+        use_dctcp=False,
+        queue_results_file="reno_queue_20.csv",
+        throughout_results_file="reno_thru_20.csv",
+        bw=1000,
+        num_flows=20,
+        time=5)
 
-    # print "queue test: dctcp - 20 flows"
-    # dctcp_queue_test(
-    #     use_dctcp=True,
-    #     queue_results_file="dctcp_queue_20.csv",
-    #     throughout_results_file="dctcp_thru_20.csv",
-    #     bw=1000,
-    #     num_flows=20,
-    #     time=5)
+    print "queue test: dctcp - 20 flows"
+    dctcp_queue_test(
+        use_dctcp=True,
+        queue_results_file="dctcp_queue_20.csv",
+        throughout_results_file="dctcp_thru_20.csv",
+        bw=1000,
+        num_flows=20,
+        time=5)
 
     dctcp_convergence_test(
         use_dctcp=True,
-        results_file="test",
+        results_file="convergence-test.txt",
         bw=100,
         num_flows=5,
         interval_time=5)
+
+    for i in range(100):
+        dctcp_queue_test(
+             use_dctcp=True,
+             queue_results_file="dctcp_k_queue.csv",
+             throughout_results_file="dctcp_k_queue.csv",
+             bw=1000,
+             k=i+1,
+             num_flows=2,
+             time=5)
 
     tcp_utils.reset_tcp()
 
