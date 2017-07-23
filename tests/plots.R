4ce5ac5a5c0052761da17cfdc78c64e1d4df9271
chowes <chowes@uwaterloo.ca>
2017-07-22 23:14:55 -0400

    Graphs for throughput, convergence, queue size

--- tests/plots.R
+++ tests/plots.R
@@ -1,12 +1,16 @@
-queue_time_graph <- function(queue_data, filename) {
+#!/usr/bin/Rscript
+
+require(ggplot2)
+
+queue_time_graph <- function(queue_data, filename, save=FALSE) {
 
   plot <- ggplot(queue_data, aes(x=time, y=q_len, colour = cong_ctl, linetype = cong_ctl)) +
     geom_line(size=1.2) + 
-    labs(x = "Time (Seconds)", y = "Queue Length") +
+    labs(x = "Time (Seconds)", y = "Queue Length (Packets)") +
     coord_cartesian(ylim = c(0, 400), xlim=c(0, 60)) +
     scale_y_continuous(expand = c(0, 0)) +
-    scale_linetype_manual(limits = c("reno_2", "dctcp_2"), breaks = c("reno_2", "dctcp_2"), values = c(3, 1), labels = c("TCP Reno", "DCTCP")) +
-    scale_color_manual(limits = c("reno_2", "dctcp_2"), breaks = c("reno_2", "dctcp_2"), values = c("black", "dark gray"), labels = c("TCP Reno", "DCTCP")) +
+    scale_linetype_manual(limits = c("reno_2", "dctcp_2"), breaks = c("reno_2", "dctcp_2"), values = c(1, 1), labels = c("TCP Reno", "DCTCP")) +
+    scale_color_manual(limits = c("reno_2", "dctcp_2"), breaks = c("reno_2", "dctcp_2"), values = c("blue", "red"), labels = c("TCP Reno", "DCTCP")) +
     theme_bw() + 
     theme(axis.title.y = element_text(size=20, margin = margin(0, 15, 0, 0), face="bold"),
           axis.title.x = element_text(size=20, margin = margin(15, 0, 0, 0), face="bold"),
@@ -24,20 +28,24 @@ queue_time_graph <- function(queue_data, filename) {
           axis.line.x = element_line(colour = 'black', size = 1.6),
           axis.line.y = element_line(colour = 'black', size = 1.6),
           plot.margin=unit(c(.5,.5,.5,.5), "cm"))
-
-  ggsave(filename=filename, width=8, height=5, plot=plot)
+  
+  if (save) {
+    ggsave(plot=plot, filename=filename, width=10, height=5)
+  } else {
+    print(plot)
+  }
 }
 
 
-queue_cdf_graph <- function(queue_data, filename) {
+queue_cdf_graph <- function(queue_data, filename, save=FALSE) {
   
-  plot <- ggplot(queue_data, aes(x=time, y=q_len, colour = cong_ctl, linetype = cong_ctl)) +
-    geom_line(size=1.2) + 
-    labs(x = "Time (Seconds)", y = "Queue Length") +
-    coord_cartesian(ylim = c(0, 400), xlim=c(0, 60)) +
+  plot <- ggplot(queue_data, aes(q_len, color=cong_ctl)) +
+    stat_ecdf(size=2) +
+    labs(x = "Queue Length (Packets)", y = "Cumulative Fraction") +
+    coord_cartesian(ylim = c(0, 1), xlim=c(0, 400)) +
     scale_y_continuous(expand = c(0, 0)) +
-    scale_linetype_manual(limits = c("reno_2", "dctcp_2"), breaks = c("reno_2", "dctcp_2"), values = c(3, 1), labels = c("TCP Reno", "DCTCP")) +
-    scale_color_manual(limits = c("reno_2", "dctcp_2"), breaks = c("reno_2", "dctcp_2"), values = c("black", "dark gray"), labels = c("TCP Reno", "DCTCP")) +
+    scale_linetype_manual(limits = c("dctcp_2", "dctcp_20", "reno_2", "reno_20"), breaks = c("dctcp_2", "dctcp_20", "reno_2", "reno_20"), values = c(1,2,4,5), labels = c("DCTCP - 2 Flows", "DCTCP - 20 Flows", "TCP - 2 Flows", "TCP - 20 Flows")) +
+    scale_color_manual(limits = c("dctcp_2", "dctcp_20", "reno_2", "reno_20"), breaks = c("dctcp_2", "dctcp_20", "reno_2", "reno_20"), values = c("red", "green", "blue", "black"), labels = c("DCTCP - 2 Flows", "DCTCP - 20 Flows", "TCP - 2 Flows", "TCP - 20 Flows")) +
     theme_bw() + 
     theme(axis.title.y = element_text(size=20, margin = margin(0, 15, 0, 0), face="bold"),
           axis.title.x = element_text(size=20, margin = margin(15, 0, 0, 0), face="bold"),
@@ -56,5 +64,73 @@ queue_cdf_graph <- function(queue_data, filename) {
           axis.line.y = element_line(colour = 'black', size = 1.6),
           plot.margin=unit(c(.5,.5,.5,.5), "cm"))
   
-  ggsave(filename=filename, width=8, height=5, plot=plot)
+  if (save) {
+    ggsave(plot=plot, filename=filename, width=10, height=5)
+  } else {
+    print(plot)
+  }
+}
+
+
+converg_graph <- function(converg_data, filename, save=FALSE) {
+  
+  plot <- ggplot(converg_data, aes(x=time, y=thru, color=sender)) +
+    geom_line(size=1) +
+    labs(x = "Time (Seconds)", y = "Throughput (Mbits/Second)") +
+    coord_cartesian(ylim = c(0, 100), xlim=c(0, 540)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_x_continuous(breaks = seq(0, 540, by = 60)) +
+    scale_color_manual(limits = c("h2", "h3", "h4", "h5", "h6"), breaks = c("h2", "h3", "h4", "h5", "h6"), values = c("purple", "red", "green", "blue", "black"), labels = c("Flow 1", "Flow 2", "Flow 3", "Flow 4", "Flow 5")) +
+    theme_bw() + 
+    theme(axis.title.y = element_text(size=20, margin = margin(0, 15, 0, 0), face="bold"),
+          axis.title.x = element_text(size=20, margin = margin(15, 0, 0, 0), face="bold"),
+          axis.text.y = element_text(size=16, color = "black"), 
+          axis.text.x = element_text(size=20, color = "black"), 
+          axis.ticks = element_line(size=1.6, color = "black"),
+          legend.text = element_text(size = 20, colour = "black", face = "bold"),
+          legend.title = element_blank(),
+          legend.key = element_blank(),
+          legend.key.size = unit(1, "cm"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(),
+          axis.line.x = element_line(colour = 'black', size = 1.6),
+          axis.line.y = element_line(colour = 'black', size = 1.6),
+          plot.margin=unit(c(.5,.5,.5,.5), "cm"))
+  
+  if (save) {
+    ggsave(plot=plot, filename=filename, width=10, height=5)
+  } else {
+    print(plot)
+  }
+}
+
+
+k_throughput_graph <- function(throughput_data, filename, save=FALSE) {
+  plot <- ggplot(throughput_data, aes(x=k, y=thru)) +
+    geom_line(size = 1.2) +
+    labs(x = "K", y = "Throughput (Mbps)") +
+    coord_cartesian(ylim = c(0, 100), xlim=c(0, 60)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_x_continuous(breaks = seq(0, 60, by = 20)) +
+    theme_bw() + 
+    theme(axis.title.y = element_text(size=20, margin = margin(0, 15, 0, 0), face="bold"),
+          axis.title.x = element_text(size=20, margin = margin(15, 0, 0, 0), face="bold"),
+          axis.text.y = element_text(size=16, color = "black"), 
+          axis.text.x = element_text(size=20, color = "black"), 
+          axis.ticks = element_line(size=1.6, color = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(),
+          axis.line.x = element_line(colour = 'black', size = 1.6),
+          axis.line.y = element_line(colour = 'black', size = 1.6),
+          plot.margin=unit(c(.5,.5,.5,.5), "cm"))
+  
+  if (save) {
+    ggsave(plot=plot, filename=filename, width=8, height=5)
+  } else {
+    print(plot)
+  }
 }
--- tests/queue_graph.R
+++ tests/queue_graph.R
@@ -1,13 +1,24 @@
 #!/usr/bin/Rscript
+
 setwd("~/Documents/School/tcp-congestion-control/")
 
 library(ggplot2)
 source("tests/plots.R")
 
 queue_data <- read.csv(file = "tests/results/queue.csv", header = TRUE)
+converg_data <- read.csv(file = "tests/results/converg.csv", header = TRUE)
+throughput_data <- read.csv(file = "tests/results/dctcp_k_thru.csv", header=TRUE)
 
-# generate graph of queue length over time - replicates figure 1 from Alizadeh et al. 2010 
+# generate graph of queue length over time
 queue_data_2_flow <- subset(queue_data, cong_ctl=='reno_2' | cong_ctl=='dctcp_2')
-queue_time_graph(queue_data_2_flow, "paper/figures/queue_2_flows.pdf")
+queue_time_graph(queue_data_2_flow, "paper/figures/queue_2_flows.pdf", save=FALSE)
 
-# generate CDF of queue length for tests with 2 and 20 flows
+# generate CDF graph of queue length for tests with 2 and 20 flows
+queue_cdf_graph(queue_data, "paper/figures/queue_cdf.pdf", save=FALSE)
+
+# generate convergance graph
+converg_graph(subset(converg_data, cong_ctl == "dctcp"), "paper/figures/dctcp_converg.pdf", save=FALSE)
+converg_graph(subset(converg_data, cong_ctl == "reno"), "paper/figures/reno_converg.pdf", save=FALSE)
+
+# generate throughput graph based on K, no effect after 20 so we can cut it off at k = 60
+k_throughput_graph(subset(throughput_data, k <= 60), "paper/figures/k_throughput.pdf", save=FALSE)
